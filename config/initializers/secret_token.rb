701b77124429d59350ebc28705fbe5ac07c95fca
Debbie Pan <xdebbiee@gmail.com>
2014-07-08 20:52:39 -0700

    first commit - adding portfolio and environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '84ae87a78d9b7411b9a16330d996c447d658a1e13479b0955943a014562c20a123504025738f7f2288c7daade6d9fa66a5c8a45c17af6f649ce72e64f8da854b'
